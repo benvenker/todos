08b26f3207b94fe53788d719fcd42c2af26d542e
Ben Venker <bvemails@gmail.com>
2014-10-28 19:22:01 -0500

    updating

--- todos.js
+++ todos.js
@@ -3,7 +3,11 @@ Todos = new Meteor.Collection('todos');
 if (Meteor.isClient) {
   Template.TodosPanel.helpers({
     items: function() {
-      return Todos.find();
+      return Todos.find({}, {
+        sort: {
+          created_at: -1
+        }
+      });
     }
   });
 
@@ -24,6 +28,23 @@ if (Meteor.isClient) {
       });
     }
   });
+
+  Template.CreateTodoItem.events({
+    'submit form': function (e, tmpl) {
+      e.preventDefault();
+
+      var subject = tmpl.find('input').value;
+
+      Todos.insert({
+        subject: subject,
+        created_at: new Date,
+        is_done: false
+      });
+
+      var form = tmpl.find('form');
+      form.reset();
+    }
+  })
 }
 
 if (Meteor.isServer) {
